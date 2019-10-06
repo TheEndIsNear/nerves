1fc80d4796a1e02ad8d3451353fa233bd8bf82ae
Greg Mefford <greg@gregmefford.com>
2019-10-06 14:37:02 -0400

    Extract ensure_available!/2 helper in Preflight

--- lib/mix/nerves/preflight.ex
+++ lib/mix/nerves/preflight.ex
@@ -4,65 +4,54 @@ defmodule Mix.Nerves.Preflight do
   def check! do
     {_, type} = :os.type()
     check_requirements("fwup")
-    check_requirements("mksquashfs")
+    ensure_available!("mksquashfs", package: "squashfs")
     check_host_requirements(type)
     Mix.Task.run("nerves.loadpaths")
   end
 
-  defp check_requirements("mksquashfs") do
-    case System.find_executable("mksquashfs") do
-      nil ->
-        Mix.raise(missing_package_message("squashfs"))
-
-      _ ->
-        :ok
-    end
-  end
-
   defp check_requirements("fwup") do
-    case System.find_executable("fwup") do
-      nil ->
-        Mix.raise(missing_package_message("fwup"))
+    ensure_available!("fwup")
 
-      _ ->
-        with {vsn, 0} <- System.cmd("fwup", ["--version"]),
-             vsn = String.trim(vsn),
-             {:ok, req} = Version.parse_requirement(@fwup_semver),
-             true <- Version.match?(vsn, req) do
-          :ok
-        else
-          false ->
-            {vsn, 0} = System.cmd("fwup", ["--version"])
+    with {vsn, 0} <- System.cmd("fwup", ["--version"]),
+         vsn = String.trim(vsn),
+         {:ok, req} = Version.parse_requirement(@fwup_semver),
+         true <- Version.match?(vsn, req) do
+      :ok
+    else
+      false ->
+        {vsn, 0} = System.cmd("fwup", ["--version"])
 
-            Mix.raise("""
-            fwup #{@fwup_semver} is required for Nerves.
+        Mix.raise("""
+        fwup #{@fwup_semver} is required for Nerves.
 
-            You are running #{vsn}.
-            Please see https://hexdocs.pm/nerves/installation.html#fwup
-            for installation instructions
-            """)
+        You are running #{vsn}.
+        Please see https://hexdocs.pm/nerves/installation.html#fwup
+        for installation instructions
+        """)
 
-          error ->
-            Mix.raise("""
-            Nerves encountered an error while checking host requirements for fwup
-            #{inspect(error)}
-            Please open a bug report for this issue on github.com/nerves-project/nerves
-            """)
-        end
+      error ->
+        Mix.raise("""
+        Nerves encountered an error while checking host requirements for fwup
+        #{inspect(error)}
+        Please open a bug report for this issue on github.com/nerves-project/nerves
+        """)
     end
   end
 
   defp check_host_requirements(:darwin) do
-    case System.find_executable("gstat") do
-      nil ->
-        Mix.raise(missing_package_message("gstat (coreutils)"))
-
-      _ ->
-        :ok
-    end
+    ensure_available!("gstat", package: "gstat (coreutils)")
   end
 
   defp check_host_requirements(_), do: nil
+
+  defp ensure_available!(executable, opts \\ []) do
+    if System.find_executable(executable) do
+      :ok
+    else
+      package = opts[:package] || executable
+      Mix.raise(missing_package_message(package))
+    end
+  end
 
   defp missing_package_message(package) do
     """
